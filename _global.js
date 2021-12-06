05ceac452c44331f8e8261c37abc2c33e6e0bfbb
unknown <khuongduy354@gmail.com>
2021-12-06 18:39:46 +0700

    timer

--- _global.js
+++ _global.js
@@ -1,5 +1,9 @@
+// const _global = {
+//   SERVER_URL: "https://chess-club-backend.herokuapp.com/",
+//   CLIENT_URL: "https://chess-club-d527d.web.app/",
+// };
 const _global = {
-  SERVER_URL: "https://chess-club-backend.herokuapp.com/",
-  CLIENT_URL: "https://chess-club-d527d.web.app/",
+  SERVER_URL: "http://localhost:8000",
+  CLIENT_URL: "http://localhost:3000",
 };
 module.exports = _global;
--- index.js
+++ index.js
@@ -187,21 +187,42 @@ io.on("connect", async (socket) => {
     if (userNameList.includes(socket.userName))
       return userNameList.pop(socket.userName);
   });
+  // room and chat
   socket.once("disconnecting", disconnecting);
   socket.on("join-room", joinRoom);
   socket.on("send-message", sendMessage);
-  socket.on("ready", ready);
-  socket.on("move", movePiece);
-  socket.on("cancel-side", cancelSide);
-  socket.on("draw", draw);
-  socket.on("checkmated", checkMated);
   socket.on("system-message", (content, roomName) => {
     io.in(roomName).emit("receive-message", {
       from: "System",
       content: content,
     });
   });
+  // pregame and mid game
+  socket.on("ready", ready);
+  socket.on("move", movePiece);
+  socket.on("cancel-side", cancelSide);
+
+  // end game
+  socket.on("draw", draw);
+  socket.on("checkmated", checkMated);
   socket.on("forfeit", forfeit);
+  socket.on("send-time", (option, side, roomName) => {
+    if (option === "reset")
+      return io
+        .in(roomName)
+        .emit("receive-time", { option: "reset", side: side });
+    if (option === "resume")
+      return io
+        .in(roomName)
+        .emit("receive-time", { option: "resume", side: side });
+    if (option === "pause")
+      return io
+        .in(roomName)
+        .emit("receive-time", { option: "pause", side: side });
+  });
+  socket.on("timeout", (lostSide, roomName) => {
+    io.in(roomName).emit("end-game", { lostSide: lostSide });
+  });
 });
 app.use(express.json());
 app.get("/", (req, res) => res.send("Hello World"));
