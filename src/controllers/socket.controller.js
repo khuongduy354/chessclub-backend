dcc6ab15fdfe2f1562dda51bd66c3931496eb336
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-02-15 11:25:10 +0700

    fix: controller instance

--- src/controllers/socket.controller.js
+++ src/controllers/socket.controller.js
@@ -6,54 +6,46 @@ const addUser = (userName) => {
   userNameList.push(userName);
 };
 const { Chess } = require("chess.js");
-
-const {
-  disconnecting,
-  joinRoom,
-  sendMessage,
-  ready,
-  movePiece,
-  cancelSide,
-  draw,
-  checkMated,
-  forfeit,
-  disconnect,
-} = {
-  disconnect: async () => {
-    const socketRooms = Array.from(socket.rooms.values());
+class SocketController {
+  constructor(io, socket) {
+    this.socket = socket;
+    this.io = io;
+  }
+  disconnect = async () => {
+    const socketRooms = Array.from(this.socket.rooms.values());
 
     //delete if no one in room
     for (const _room of socketRooms) {
-      let sockets = await io.in(_room).fetchSockets();
+      let sockets = await this.io.in(_room).fetchSockets();
       if (!sockets.length) {
         await Room.deleteOne({ name: _room });
       }
     }
 
     // delete user from list
-    if (userNameList.includes(socket.userName))
-      return userNameList.pop(socket.userName);
-  },
+    if (userNameList.includes(this.socket.userName))
+      return userNameList.pop(this.socket.userName);
+  };
 
-  disconnecting: async () => {},
-  joinRoom: async (userName, roomName, cb) => {
+  disconnecting = async () => {};
+  joinRoom = async (userName, roomName, cb) => {
     try {
       // track user
       if (userNameList.includes(userName))
         return cb({ error: "User available" });
-      socket.userName = userName;
+      this.socket.userName = userName;
       addUser(userName);
 
       // join room
       const roomExist = await Room.findOne({ name: roomName });
       if (roomExist) {
-        socket.join(roomName);
+        this.socket.join(roomName);
         return;
       } else {
         const { result, error } = await createRoom(roomName);
         if (error) return cb({ error: "Can't create room" });
-        socket.join(roomName);
-        io.in(roomName).emit("receive-message", {
+        this.socket.join(roomName);
+        this.io.in(roomName).emit("receive-message", {
           from: "System",
           content: `${userName} has joined`,
         });
@@ -62,16 +54,16 @@ const {
       console.log(e);
       cb({ error: "Cannot create room" });
     }
-  },
-  sendMessage: (msg, roomName, userName) => {
+  };
+  sendMessage = (msg, roomName, userName) => {
     const response = {
       from: userName,
       content: msg,
     };
-    io.in(roomName).emit("receive-message", response);
-  },
+    this.io.in(roomName).emit("receive-message", response);
+  };
 
-  ready: async (userName, roomName, side, cb) => {
+  ready = async (userName, roomName, side, cb) => {
     const room = await Room.findOne({ name: roomName });
     try {
       //  assign player to black or white slot if empty
@@ -84,7 +76,7 @@ const {
           { black: userName },
           { new: true }
         );
-        io.in(roomName).emit("chose-black", userName);
+        this.io.in(roomName).emit("chose-black", userName);
       }
       if (side == "white") {
         const updated = await Room.findOneAndUpdate(
@@ -92,11 +84,11 @@ const {
           { white: userName },
           { new: true }
         );
-        io.in(roomName).emit("chose-white", userName);
+        this.io.in(roomName).emit("chose-white", userName);
       }
 
       // notify
-      io.in(roomName).emit("receive-message", {
+      this.io.in(roomName).emit("receive-message", {
         from: "System",
         content: `${userName} chose ${side}`,
       });
@@ -105,20 +97,20 @@ const {
       //   start the game if both black and white are occupied
       const newRoom = await Room.findOne({ name: roomName });
       if (newRoom.black && newRoom.white) {
-        io.in(roomName).emit("start-game");
+        this.io.in(roomName).emit("start-game");
         const newGame = new Chess();
         await Room.findOneAndUpdate(
           { name: roomName },
           { game: newGame.fen() }
         );
-        io.to(roomName).emit("update-game", newGame.fen());
+        this.io.to(roomName).emit("update-game", newGame.fen());
       }
     } catch (e) {
       console.log(e);
       cb({ error: "Unable to pick side" });
     }
-  },
-  movePiece: (movedFen, roomName, cb) => {
+  };
+  movePiece = (movedFen, roomName, cb) => {
     try {
       // const updated = await Room.findOneAndUpdate(
       //   { name: roomName },
@@ -127,63 +119,52 @@ const {
       //   },
       //   { new: true }
       // );
-      io.in(roomName).emit("update-game", movedFen);
+      this.io.in(roomName).emit("update-game", movedFen);
     } catch (e) {
       console.log(e);
       cb({ error: "Cant not make move" });
     }
-  },
-  cancelSide: async (side, roomName) => {
+  };
+  cancelSide = async (side, roomName) => {
     // cancel side selection
     if (side === "black") {
       await Room.findOneAndUpdate({ name: roomName }, { black: null });
-      io.in(roomName).emit("chose-black", null);
+      this.io.in(roomName).emit("chose-black", null);
     }
     if (side === "white") {
       await Room.findOneAndUpdate({ name: roomName }, { white: null });
-      io.in(roomName).emit("chose-white", null);
+      this.io.in(roomName).emit("chose-white", null);
     }
-  },
-  draw: async (roomName) => {
+  };
+  draw = async (roomName) => {
     const room = await Room.findOneAndUpdate(
       { name: roomName },
       { black: null, white: null }
     );
-    io.in(roomName).emit("chose-black", null);
-    io.in(roomName).emit("chose-white", null);
-    io.in(roomName).emit("end-game", { draw: true });
-  },
-  checkMated: async (lostSide, roomName) => {
+    this.io.in(roomName).emit("chose-black", null);
+    this.io.in(roomName).emit("chose-white", null);
+    this.io.in(roomName).emit("end-game", { draw: true });
+  };
+  checkMated = async (lostSide, roomName) => {
     const room = await Room.findOneAndUpdate(
       { name: roomName },
       { black: null, white: null }
     );
-    io.in(roomName).emit("chose-black", null);
-    io.in(roomName).emit("chose-white", null);
-    io.in(roomName).emit("end-game", { lostSide: lostSide });
-  },
-  forfeit: async (userName, roomName, side) => {
+    this.io.in(roomName).emit("chose-black", null);
+    this.io.in(roomName).emit("chose-white", null);
+    this.io.in(roomName).emit("end-game", { lostSide: lostSide });
+  };
+  forfeit = async (userName, roomName, side) => {
     if (side === "white")
       await Room.findOneAndUpdate({ name: roomName }, { white: null });
     if (side === "black")
       await Room.findOneAndUpdate({ name: roomName }, { black: null });
-    io.in(roomName).emit("forfeit", side);
-    io.in(roomName).emit("receive-message", {
+    this.io.in(roomName).emit("forfeit", side);
+    this.io.in(roomName).emit("receive-message", {
       from: "System",
       content: `${userName} forfeited`,
     });
-  },
-};
+  };
+}
 
-module.exports = {
-  disconnecting,
-  joinRoom,
-  sendMessage,
-  ready,
-  movePiece,
-  cancelSide,
-  draw,
-  checkMated,
-  forfeit,
-  disconnect,
-};
+module.exports = { SocketController };
--- src/routes/socket.route.js
+++ src/routes/socket.route.js
@@ -1,37 +1,27 @@
-const {
-  disconnecting,
-  joinRoom,
-  sendMessage,
-  ready,
-  movePiece,
-  cancelSide,
-  draw,
-  checkMated,
-  forfeit,
-  disconnect,
-} = require("../controllers/socket.controller");
-
+const { SocketController } = require("../controllers/socket.controller");
 const setupSocketRoute = (io) => {
   io.on("connect", async (socket) => {
-    socket.on("disconnect", disconnect);
+    const controller = new SocketController(io, socket);
+
+    socket.on("disconnect", controller.disconnect);
     // room and chat
     // socket.once("disconnecting", disconnecting);
-    socket.on("join-room", joinRoom);
-    socket.on("send-message", sendMessage);
+    socket.on("join-room", controller.joinRoom);
+    socket.on("send-message", controller.sendMessage);
     // socket.on("system-message", (content, roomName) => {
     //   io.in(roomName).emit("receive-message", {
     //     from: "System",
     //     content: content,
     //   });
     // pregame and mid game
-    socket.on("ready", ready);
-    socket.on("move", movePiece);
-    socket.on("cancel-side", cancelSide);
+    socket.on("ready", controller.ready);
+    socket.on("move", controller.movePiece);
+    socket.on("cancel-side", controller.cancelSide);
 
     // end game
-    socket.on("draw", draw);
-    socket.on("checkmated", checkMated);
-    socket.on("forfeit", forfeit);
+    socket.on("draw", controller.draw);
+    socket.on("checkmated", controller.checkMated);
+    socket.on("forfeit", controller.forfeit);
 
     // socket.on("send-time", (option, side, roomName) => {
     //   if (option === "reset")
